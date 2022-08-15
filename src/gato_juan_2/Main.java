6fa014785dfe63833fec5d0c775c1962df7e44f0
Juan Granados <111211763+1105agramon@users.noreply.github.com>
2022-08-14 19:58:27 -0500

    Update Main.java
    Es una prueba de comic

--- src/gato_juan_2/Main.java
+++ src/gato_juan_2/Main.java
@@ -2,6 +2,7 @@ package gato_juan_2;
 public class Main {
     public static void main(String[] args) {
         Menu m=new Menu();
+        System.out.println("Es una prueba");
         m.menuPrincipal();
     }
 }
